d7adf1f72837c38a59072f20a907e1a49c132a11
Alexander <80062043+Susurmi@users.noreply.github.com>
2023-08-15 21:08:15 +0200

    event handler
    added basic event handler.
    moved ready event to own ready.js file in /events.

---
+++ events/ready.js
@@ -0,0 +1,14 @@
+const { Events, Client } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+
+	/**
+	 * @param {Client} client
+	 */
+
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.username}.`);
+	},
+};
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 require('dotenv').config();
+const path = require('path');
+const fs = require('fs');
 const { Client, GatewayIntentBits, Events } = require('discord.js');
 
 const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-bot.once(Events.ClientReady, (c) => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const loadEvents = () => {
+	const eventsPath = path.join(__dirname, '/events');
+	const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
-bot.login(process.env.BOT_TOKEN);
+	for (const file of eventFiles) {
+		const filePath = path.join(eventsPath, file);
+		const event = require(filePath);
+		if (event.once) {
+			bot.once(event.name, (...args) => event.execute(...args));
+		} else {
+			bot.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+};
+
+(() => {
+	loadEvents();
+
+	const token = process.env.BOT_TOKEN;
+	bot.login(token);
+})();
