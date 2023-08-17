1f8fbb40d29c0d3a9a2abb98f7f6eea417a14f2d
Alexander <80062043+Susurmi@users.noreply.github.com>
2023-08-17 18:52:09 +0200

    update status command
    added bot uptime.
    created a status embed that also contains info like the bot id.

--- commands/devTools/status.js
+++ commands/devTools/status.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder, Interaction, PermissionFlagsBits } = require('discord.js');
+const {
+	SlashCommandBuilder,
+	Interaction,
+	PermissionFlagsBits,
+	EmbedBuilder,
+} = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,15 +15,52 @@ module.exports = {
 	 * @param {Interaction} interaction
 	 */
 
-	async execute(interaction, client) {
+	async execute(interaction) {
 		await interaction.deferReply();
 
 		try {
 			const reply = await interaction.fetchReply();
 
+			const icon = interaction.client.user.displayAvatarURL();
+
 			const ping = reply.createdTimestamp - interaction.createdTimestamp;
 
-			interaction.editReply(`Pong! Client ${ping}ms || Websocket: ${interaction.client.ws.ping}ms`);
+			// Create Bot Uptime String
+			const totalSecondsUptime = Math.floor(interaction.client.uptime / 1000);
+			const days = Math.floor(totalSecondsUptime / 86400);
+			const hours = Math.floor((totalSecondsUptime % 86400) / 3600);
+			const minutes = Math.floor((totalSecondsUptime % 3600) / 60);
+			const seconds = totalSecondsUptime % 60;
+			let uptimeString = '';
+
+			if (days > 0) {
+				uptimeString += `${days} day${days !== 1 ? 's' : ''} `;
+			}
+			if (hours > 0) {
+				uptimeString += `${hours} hour${hours !== 1 ? 's' : ''} `;
+			}
+			if (minutes > 0) {
+				uptimeString += `${minutes} minute${minutes !== 1 ? 's' : ''} `;
+			}
+			if (seconds > 0 || uptimeString === '') {
+				uptimeString += `${seconds} second${seconds !== 1 ? 's' : ''} `;
+			}
+
+			const statusEmbed = new EmbedBuilder()
+				.setAuthor({ name: `${interaction.client.user.username}`, iconURL: icon })
+				.setThumbnail(`${icon}`)
+				.setColor('Random')
+				.addFields({ name: 'Client Latency', value: `${ping}ms`, inline: true })
+				.addFields({
+					name: 'WebSocket Latency',
+					value: `${interaction.client.ws.ping}ms`,
+					inline: true,
+				})
+				.addFields({ name: 'Uptime', value: `${uptimeString}` })
+				.setFooter({ text: `Bot ID: ${interaction.client.application.id}` })
+				.setTimestamp();
+
+			interaction.editReply({ embeds: [statusEmbed] });
 		} catch (error) {
 			console.error(error);
 		}
