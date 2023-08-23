34a3bafb2b7e6c9fba7581ec692ce62380578775
Alexander <80062043+Susurmi@users.noreply.github.com>
2023-08-23 21:02:53 +0200

    added weather command
    added a weather command using openweather api.
    renamed devTools directory to just dev.
    added axios package.
    weather command needs an api key which has to be in the .env file.

---
+++ commands/misc/weather.js
@@ -0,0 +1,94 @@
+require('dotenv').config({ path: '../../../.env' });
+const axios = require('axios');
+const { SlashCommandBuilder, EmbedBuilder, CommandInteraction } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('weather')
+		.setDescription(
+			'shows an embeded message with the current weather information of the provided location.',
+		)
+		.addStringOption((option) =>
+			option
+				.setName('city')
+				.setDescription('provid the location you want to know the weather of.')
+				.setRequired(true),
+		),
+
+	/**
+	 * @param {CommandInteraction} interaction
+	 */
+
+	async execute(interaction) {
+		const degToText = (degree) => {
+			if (degree > 337.5) return 'Northerly ⬆';
+			if (degree > 292.5) return 'North Westerly ↖';
+			if (degree > 247.5) return 'Westerly ⬅';
+			if (degree > 202.5) return 'South Westerly ↙';
+			if (degree > 157.5) return 'Southerly ⬇';
+			if (degree > 122.5) return 'South Easterly ↘';
+			if (degree > 67.5) return 'Easterly ➡';
+			if (degree > 22.5) {
+				return 'North Easterly ↗';
+			}
+			return 'Northerly ⬆';
+		};
+		const deUmlaut = (word) => {
+			word = word.replace(/ä/g, 'ae');
+			word = word.replace(/ö/g, 'oe');
+			word = word.replace(/ü/g, 'ue');
+			word = word.replace(/ß/g, 'ss');
+			return word;
+		};
+		const city = deUmlaut(interaction.options.getString('city'));
+		const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
+		axios({
+			method: 'get',
+			url: apiUrl,
+		})
+			.then(async (response) => {
+				const embed = new EmbedBuilder()
+					.setTitle(`Weather in **${response.data.name}**`)
+					.setColor('Default')
+					.setThumbnail(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+					.addFields(
+						{
+							name: 'Degrees',
+							value: `${Math.floor(response.data.main.temp)}°C`,
+							inline: true,
+						},
+						{
+							name: 'Weather',
+							value: `${response.data.weather[0].main}`,
+							inline: true,
+						},
+						{
+							name: 'Humidity',
+							value: `${response.data.main.humidity}%`,
+							inline: true,
+						},
+					)
+					.addFields(
+						{
+							name: 'Wind Speed',
+							value: `${response.data.wind.speed} meter/sec`,
+							inline: true,
+						},
+						{
+							name: 'Wind Direction',
+							value: `${degToText(response.data.wind.deg)}`,
+							inline: true,
+						},
+						{
+							name: 'Measured',
+							value: `<t:${response.data.dt}:F>`,
+							inline: true,
+						},
+					);
+				await interaction.reply({ embeds: [embed] });
+			})
+			.catch(async (error) => {
+				console.error(error);
+			});
+	},
+};
--- events/botReady.js
+++ events/botReady.js
@@ -9,6 +9,8 @@ module.exports = {
 	 */
 
 	execute(client) {
+		client.user.setUsername(process.env.BOT_NAME);
+		client.user.setPresence({ activities: [{ name: 'SlashCommands!' }], status: 'online' });
 		console.log(`Ready! Logged in as ${client.user.username}.`);
 	},
 };
